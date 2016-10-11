6c35c9129188f0bb089a0cf106ab0ca0f710f2ef
Masashi Kaneko <kmasash1@Takeda-no-MacBook-Pro.local>
2016-10-11 14:30:54 +0900

    Your Trips Controller+View: Completed

--- AirAlien/db/migrate/20150820111854_add_extra_fields_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddExtraFieldsToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :phone_number, :string
-    add_column :users, :description, :text
-  end
-end
--- AirAlien/db/migrate/20150902061642_add_fields_to_room.rb
+++
@@ -1,6 +0,0 @@
-class AddFieldsToRoom < ActiveRecord::Migration
-  def change
-    add_column :rooms, :latitude, :float
-    add_column :rooms, :longitude, :float
-  end
-end
